058d8991de7747a5600f4d64e7a4a75159718ed4
Gene Rush <erush91@gmail.com>
2019-12-14 18:15:07 -0500

    removed the build_workspace from src

--- src/build_workspace.sh
+++
@@ -1,7 +0,0 @@
-catkin_make --pkg driver_common
-catkin_make --pkg dynamixel_msgs
-catkin_make --pkg dynamixel_controllers
-catkin_make --pkg driver_base
-catkin_make --pkg hokuyo_node
-catkin_make --pkg rplidar_ros
-catkin_make --pkg spin_lidar
